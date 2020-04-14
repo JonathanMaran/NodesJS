b9c799051bddd0a0cb9920026a666b2815e71c50
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-04-14 15:42:29 +0200

    NPM commentaires

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function (err, resu
                 skytext,
                 temperature
             }
-    } = result[0];
+    } = result[0]; // le résultat est dans la variable result, c'est un tableau donc on parcourt avec [ ]
 
     console.log(skytext);
     console.log(temperature);
