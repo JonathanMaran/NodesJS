1aaaeda005b0fa57226c7101699f2edebc41a150
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2020-04-14 15:32:28 +0200

    NPM

---
+++ script.js
@@ -0,0 +1,20 @@
+var weather = require('weather-js');
+
+// Options:
+// search:     location name or zipcode
+// degreeType: F or C
+
+weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function (err, result) {
+    if (err) console.log(err);
+
+    let {
+        current:
+            {
+                skytext,
+                temperature
+            }
+    } = result[0];
+
+    console.log(skytext);
+    console.log(temperature);
+});
